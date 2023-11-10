0fb9e86c30a8c5499a09108252dd0c4bee5cb1d3
David Cates <david.cates@networktocode.com>
2023-11-09 21:18:58 -0700

    update jobs

--- jobs/demo_jobs.py
+++ jobs/demo_jobs.py
@@ -1,5 +1,5 @@
 from nautobot.core.celery import register_jobs
-from nautobot.extras.jobs import Job
+from nautobot.extras.jobs import BooleanVar, Job
 
 
 name = "Demo Jobs for Nautobot 2.0"
@@ -11,6 +11,8 @@ class DemoJob(Job):
     class Meta:
         name = "Create Site"
         description = "A job used to demonstrate the jobs feature of Nautobot"
+
+    debug = BooleanVar(description="Enable for more verbose debug logging", default=False)
 
     def run(self, debug):
         self.debug = debug
