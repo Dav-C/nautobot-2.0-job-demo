c7fda7102cba93fb2a13ddc07f31a12eb7cad468
David Cates <david.cates@networktocode.com>
2023-11-09 21:05:18 -0700

    update jobs

---
+++ demo_jobs.py
@@ -0,0 +1,22 @@
+from nautobot.core.celery import register_jobs
+from nautobot.extras.jobs import Job
+
+
+name = "Demo Jobs for Nautobot 2.0"
+
+
+class DemoJob(Job):
+    """A Demo Job."""
+
+    class Meta:
+        name = "Create Site"
+        description = "A job used to demonstrate the jobs feature of Nautobot"
+
+    def run(self, debug):
+        self.debug = debug
+
+        self.logger.info("The job ran successfully.")
+
+
+jobs = [DemoJob]
+register_jobs(*jobs)
